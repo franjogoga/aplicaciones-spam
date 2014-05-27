5f385e93b5b710c3687c666421bbe7b027a3fd26
jonatan.gonzales@pucp.pe <jonatan.gonzales@pucp.pe@456ce11d-b9ae-6d99-dca1-5e30e6395189>
2014-05-27 11:49:18 +0000

    buena

--- AplicaSpam/src/Main.java
+++ AplicaSpam/src/Main.java
@@ -4,9 +4,29 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) throws Exception{
+		
+		ArrayList<String> clavesList = getListaClaves("/home/jonatan/workspace/claves.txt");
+		
+		for (int i=0; i<4292; i++) {						
+			if (i>=0 && i<=9) {			
+				getCaracteristicas(i,"/home/jonatan/workspace/extraidoTEST/TEST_0000"+i+".eml", clavesList);				
+			}			
+			if (i>=10 && i<=99) {
+				getCaracteristicas(i,"/home/jonatan/workspace/extraidoTEST/TEST_000"+i+".eml", clavesList);
+			}
+			if (i>=100 && i<=999) {
+				getCaracteristicas(i,"/home/jonatan/workspace/extraidoTEST/TEST_00"+i+".eml", clavesList);
+			}
+			if (i>=1000 && i<=9999) {
+				getCaracteristicas(i,"/home/jonatan/workspace/extraidoTEST/TEST_0"+i+".eml", clavesList);
+			}							
+		}
+	}	
+	
+	public static ArrayList<String> getListaClaves(String clavesArch) {
 		ArrayList<String> clavesList = new ArrayList<String>();
 		try {
-			FileInputStream fstream = new FileInputStream("/home/jonatan/workspace/claves.txt");
+			FileInputStream fstream = new FileInputStream(clavesArch);
 			DataInputStream entrada = new DataInputStream(fstream);
 			BufferedReader buffer = new BufferedReader(new InputStreamReader(entrada));			
 			String strLinea;
@@ -18,41 +38,30 @@ public class Main {
 			
 		} catch (Exception e) {
 			System.err.println("Ocurrio un error: " + e.getMessage());
-		}			
+		}		
+		return clavesList;
+	}
+	
+	public static void getCaracteristicas(int i, String correoArch, ArrayList<String> clavesList) throws Exception {
+		File file = new File(correoArch);		
+		Scanner scanFile = new Scanner(new FileReader(file));						
+				 		
+		String theWord, contenido="";
+		int cantPalabras=0, cantClaves=0;
+		while (scanFile.hasNext()){		
+		    theWord = scanFile.next();
+		    contenido=contenido.toLowerCase()+" "+theWord;				    				    		    				   
+		    cantPalabras++;				    
+		}				
+		scanFile.close();
 		
-		for (int i=0; i<4292; i++) {
-			
-			if (i>=0 && i<=9) {			
-				File file = new File("/home/jonatan/workspace/extraidoTEST/TEST_0000"+i+".eml");		
-				Scanner scanFile = new Scanner(new FileReader(file));						
-						 		
-				String theWord, contenido="";
-				int cantPalabras=0, cantClaves=0;
-				while (scanFile.hasNext()){		
-				    theWord = scanFile.next();
-				    contenido=contenido.toLowerCase()+" "+theWord;				    				    		    				   
-				    cantPalabras++;				    
-				}				
-				scanFile.close();
-				
-				for(int j=0; j<clavesList.size(); j++) {
-			    	if (contenido.contains(clavesList.get(j)))
-			    		cantClaves++;
-			    }
-				
-				System.out.println("Correo:"+i+"\tPalabras:"+cantPalabras+"\t\tFrases clave:"+cantClaves);
-			}
-			
-			if (i>=10 && i<=99) {
-				
-			}
-			if (i>=100 && i<=999) {
-				
-			}
-			if (i>=1000 && i<=9999) {
-				
-			}							
-		}
-	}	
+		for(int j=0; j<clavesList.size(); j++) {
+	    	if (contenido.contains(clavesList.get(j)))
+	    		cantClaves++;
+	    }
+		
+		System.out.println("Correo:"+i+"\tPalabras:"+cantPalabras+"\t\tFrases clave:"+cantClaves);
+	}
+	
 
 }
